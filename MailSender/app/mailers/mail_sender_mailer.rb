0afd1a67ba9a06ec5985bedaf00c289c87971cee
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-11-08 11:06:22 -0500

    UPDATE: comment

--- MailSender/app/mailers/mail_sender_mailer.rb
+++ MailSender/app/mailers/mail_sender_mailer.rb
@@ -1,5 +1,6 @@
 class MailSenderMailer < ApplicationMailer::Base
     default from: "from@example.com"
+    # take user parameters and sends email using method mail
     def send_email(user)
         @user = user
         mail(to: @user.email, subject: 'Mail from Rails')
